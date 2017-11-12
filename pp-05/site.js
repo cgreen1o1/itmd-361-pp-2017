6fc70531cc196997cf3462afb25fdc25336dc6d8
Christine Lode <christine.lode@gmail.com>
2017-11-11 19:33:56 -0600

    Add/declare variables for audio and image

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,6 +1,10 @@
 $.noConflict();
 
 (function($) {
+  var doorbell = new Audio('media/doorbell.mp3');
+  var image = new Image();
+  image.src= 'media/doorbell.png';
+
   $('html').removeClass('nojs').addClass('js');
 
   function ringDoorbell() {
