ecee7e8c319970012c3b1ae44cda1ebac26853aa
Christine Lode <christine.lode@gmail.com>
2017-11-11 18:52:13 -0600

    Re-introduce dollar sign scoped to self-executing function. Add js class using jQuery

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,6 +1,11 @@
 $.noConflict();
 
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+(function($) {
+  $('html').removeClass('nojs').addClass('js');
+
+  function ringDoorbell() {
+    var doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
+})(jQuery);
